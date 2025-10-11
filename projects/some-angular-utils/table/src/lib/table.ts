c391d95c65276e5e9a77092dbfdce9882ee03b71
José de los santos <delossantosramosjose@gmail.com>
2025-10-11 23:22:59 +0200

    add projects/some-angular-utils/table

---
+++ projects/some-angular-utils/table/src/lib/table.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-table',
+  imports: [],
+  template: `
+    <p>
+      table works!
+    </p>
+  `,
+  styles: ``
+})
+export class TableModule {
+
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TableModule } from '@some-angular-utils/table';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TableModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
